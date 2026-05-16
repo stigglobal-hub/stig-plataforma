a079d7c2aa29fa8c1dd49688a647ba573a680590
stigglobal-hub <stig.global@gmail.com>
2026-05-16 13:53:11 -0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,9 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body style={{ margin: 0, padding: 0 }}>
+        {children}
+      </body>
+    </html>
+  );
+}
